f9686adf0034abfea7faf13c8c9dd570aa07aaac
Yennifer Cell <yennifer.cell@example.com>
2026-06-10 18:21:08 +0300

    updated book.py and coffee.py, added compiled files

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+            return
+        self._page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if size not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large")
+            return
+        self._size = size
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
